473ba9633e30c1f89b9a347a25a94f37eb3d7b86
tundedev2022 <togunbambi1@gmail.com>
2022-07-02 01:31:03 +0100

    first commit

---
+++ Assignment.cs
@@ -0,0 +1,27 @@
+using system;
+
+using system.Text;
+
+using system.Collections.Text;
+
+using system.Ling;
+
+using system.Threading.Tasks;
+
+namespace CSC_ASS{
+    class program{
+
+        static void Main(string[] args){
+
+string yourUserName;
+
+console.WriteLine ("");
+
+yourUserName= Console.Readline();
+
+console.WriteLine("Your UserName is {0},yourUserName");
+
+console.WriteLine("YourUserName DataType is {0},yourUser Name.GetType()");
+      }
+        }   
+            }
---
+++ Sample.cs
@@ -0,0 +1,28 @@
+using system;
+
+using system.Text;
+
+using system.Collections.Text;
+
+using system.Ling;
+
+using system.Threading.Tasks;
+
+namespace CSC_ASS{
+    class program{
+
+        static void Main(string[] args){
+
+int yourMatricNumber;
+
+console.WriteLine ("Enter Your Matric Number");
+
+yourMatrickNumber= Console.Readline();
+
+console.WriteLine("Your Matric Number is {0},yourMatricNumber");
+
+console.WriteLine("Your Matric Number DataType is {0},yourMatric Number.GetType()");
+      }
+        }   
+            }
+   
